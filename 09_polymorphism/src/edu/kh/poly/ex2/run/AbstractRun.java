2a420091c931971b74373e765662ef41be2f2d38
shinmyoungha <audgk0309@naver.com>
2025-02-26 12:45:25 +0900

    다형성 + 메서드

---
+++ 09_polymorphism/src/edu/kh/poly/ex2/run/AbstractRun.java
@@ -0,0 +1,12 @@
+package edu.kh.poly.ex2.run;
+
+import edu.kh.poly.ex2.model.service.AbstractService;
+
+public class AbstractRun {
+
+	public static void main(String[] args) {
+		new AbstractService().ex1();
+
+	}
+
+}
